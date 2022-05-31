b88b6cd1917fa1d74a2a1b6e07d2d2f455d4401b
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-05-31 11:55:52 -0600

    Updated Post, started creating the patch for reviews

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function getMovies() {
 function iterateMovies(movies){
     movies.forEach(movie => {
         displayMovie(movie) //Add Movie to the sidebar
-        //postMovieReference(movie) //Create the user JSON db
+        //postMovieReference(movie) //Create the user JSON db - only needs to be created once
     })
 };
 
@@ -26,31 +26,89 @@ function displayMovie(movie){
 };
 
 //Create the user JSON db
-// function postMovieReference(movie){
-//     console.log(movie);
+//  function postMovieReference(movie){
+//      console.log(movie);
 
 //     const newMovieForUser = {
-//         movieId: movie.id,
+//         id: movie.id,
 //         watched: null,
 //         rating: null,
 //         comment: [],
 //     }
 
-    // fetch(`http://localhost:3000/movies`, {
-    //     method: 'POST',
-    //     headers: {
-    //         "content-type": "application/json",
-    //         "Accept": "application/json"
-    //     },
-    //     body: JSON.stringify(newMovieForUser)
-    // })
-// };
+//     fetch(`http://localhost:3000/movies`, {
+//         method: 'POST',
+//         headers: {
+//             "content-type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify(newMovieForUser)
+//     })
+//  };
+
+function getMovieReview(movieId) {
+    fetch(`http://localhost:3000/movies/${movieId}`)
+    .then(resp => resp.json())
+    .then(data => {
+        displayReview(data);
+    })
+};
 
 
 //Add submit event for review form
 function addMovieReviewEvent(){
-
+    const form = document.getElementById('user-review');
+    form.addEventListener('submit', parseReview)
 };
+
+function parseReview(event){
+    event.preventDefault();
+
+    // const movieTitle = document.getElementById('');
+    // const movieId = movieTitle.dataset.num.textContent;
+    const movieId = "12cfb892-aac0-4c5b-94af-521852e46d6a"
+
+    let rating = event.target.rating.value;
+    let comment = event.target["new-comment"].value;
+
+    if (rating === '') {
+        rating = 'NR';
+    }
+
+    if (comment === '') {
+        comment = null;
+    }
+
+    patchReview(rating, comment, movieId);
+}
+
+function patchReview(rating, comment, movieId) {
+    const review = {
+        rating: rating,
+        comment: comment
+    }
+
+  fetch(`http://localhost:3000/movies/${movieId}`, {
+    method: "PATCH",
+    headers: {
+        "content-type": "application/json",
+        "Accept": "application/json"
+        },
+    body: JSON.stringify(review)
+    } 
+  )
+  .then(resp => resp.json())
+  .then(data => {
+      //console.log(data);
+      displayMovieRating(data);
+      displayReview(data);
+  })
+};
+
+function displayMovieRating(data){};
+
+function displayReview(data){};
+
 
 //Add click event to begin randomize leftover movies
 function addMovieRandomizerEvent(){
